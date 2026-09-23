1c34b221709ae22962acae8a7abf208d93d5e9b7
sdaniel1335 <s@daniel1335.com>
2026-09-23 23:49:32 +0200

    #1

--- private_html/app/config/app.php
+++ private_html/app/config/app.php
@@ -1,9 +1,10 @@
 <?php
 define('APP_NAME', 'Playlist');
+define('APP_PROJECT_URL', 'https://github.com/sdaniel1335/playlist');
 define('APP_VER', '0.1.0');
 define('APP_TZ', 'UTC');
 define('APP_ENV', 'dev'); // prod
 define('APP_LANG', 'en');
-define('APP_TITLE', 'App');
+define('APP_TITLE', 'Playlist');
 define('APP_SESSION', true); // flash
 // define('APP_URL', 'https://example.com');
