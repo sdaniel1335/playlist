d01497a1091e293247ef6d9573fc71d6760c6027
sdaniel1335 <s@daniel1335.com>
2026-09-26 00:52:37 +0200

    Change app version from 0.2.0 to 0.1.0

--- private_html/app/config/app.php
+++ private_html/app/config/app.php
@@ -1,7 +1,7 @@
 <?php
 define('APP_NAME', 'Playlist');
 define('APP_PROJECT_URL', 'https://github.com/sdaniel1335/playlist');
-define('APP_VER', '0.2.0');
+define('APP_VER', '0.1.0');
 define('APP_TZ', 'UTC');
 define('APP_ENV', 'dev'); // prod
 define('APP_LANG', 'en');
